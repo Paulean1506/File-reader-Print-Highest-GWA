fdaf28301703be5f517df2025a8909f45339d931
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-04-27 16:08:08 +0800

    header design

---
+++ File_reader_Print_Highest_GWA.py
@@ -0,0 +1,11 @@
+# Paulean Marguerette F. Parrish
+# Bscpe 1-4
+# Problem no. 1
+# Write a Python program that read a file containing the name of 20 students together with their GWA. 
+# The program will outputs the name of the student who got the highest GWA (including the GWA).
+
+# Header design
+import pyfiglet
+
+print(" \033[95mHello! I hope you're having a great day!!! ".center(70, "="))
+print("")
